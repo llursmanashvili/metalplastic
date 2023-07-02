01a1fac0c9f188dfb12083e74a9189cd37a736d3
llursmanashvili <lukacho.lursmanashvili+t2@gmail.com>
2023-07-02 09:52:30 +0400

    new photos

--- src/components/body/photo.js
+++ src/components/body/photo.js
@@ -9,6 +9,12 @@ import ml5 from "./photos/ml8.jpg";
 import ml8 from "./photos/ml12.jpg";
 import ml10 from "./photos/ml14.jpg";
 import ml11 from "./photos/ml15.jpg";
+import ml12 from "./photos/ml16.jpeg";
+import ml13 from "./photos/ml17.jpeg";
+import ml14 from "./photos/ml18.jpg";
+import ml15 from "./photos/ml19.jpg";
+import ml16 from "./photos/ml20.jpg";
+
 import { useEffect } from "react";
 
 import "./photo.css";
@@ -18,9 +24,15 @@ function Photo() {
     window.scrollTo(0, 0);
   }, []);
   const photos = [
+    { id: 13, image: ml12 },
+    { id: 15, image: ml14 },
+    { id: 14, image: ml13 },
+    { id: 16, image: ml15 },
+
+    { id: 17, image: ml16 },
+
     { id: 11, image: ml10 },
     { id: 12, image: ml11 },
-
     { id: 2, image: ml1 },
     { id: 3, image: ml2 },
     { id: 4, image: ml3 },
